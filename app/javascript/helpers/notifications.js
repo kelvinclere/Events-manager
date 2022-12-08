3d230badfe764fe744276d7ea3d6cd2569f76a84
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-12-08 14:43:12 +0300

    added error render

--- app/javascript/helpers/notifications.js
+++ app/javascript/helpers/notifications.js
@@ -1,16 +1,17 @@
 import { toast, Flip } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const defaults = {
-  position: 'top-right',
-  autoClose: 5000,
-  hideProgressBar: true,
-  closeOnClick: true,
-  pauseOnHover: true,
-  draggable: true,
-  progress: undefined,
-  transition: Flip,
-};
+// const defaults = {
+//   position: 'top-right',
+//   padding: 20,
+//   autoClose: 1000,
+//   hideProgressBar: true,
+//   closeOnClick: true,
+//   pauseOnHover: true,
+//   draggable: true,
+//   progress: undefined,
+//   transition: Flip,
+// };
 
 export const success = (message, options = {}) => {
   toast.success(message, Object.assign(defaults, options));
